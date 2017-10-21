20eaab23abc1828d055958d91b4cf597a7e33c3c
Timothy Cevallos <timcev@gmail.com>
2017-10-21 17:12:42 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,17 +1,17 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-	self.maximum(:rating)
+	   self.maximum(:rating)
   end
 
   def self.most_popular_show
-    self.find_by(highest_rating) 
+    self.find_by(highest_rating)
   end
-     
+
   def self.lowest_rating
 	   self.minimum(:rating)
   end
-    
+
   def self.least_popular_show
     self.where(rating: lowest_rating).first
   end
